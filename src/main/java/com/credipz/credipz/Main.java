ad6eb4c8c2c9d93f07733c00bedb4a0a78982615
zamora <zamora@ZAMORA.clearpath.com>
2019-11-21 11:49:18 -0300

    MODIFIQUE LINEA DE HOLA MUNDO

--- src/main/java/com/credipz/credipz/Main.java
+++ src/main/java/com/credipz/credipz/Main.java
@@ -16,7 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HOLA mundo");
+        System.out.println("HOLA mundo...");
         // TODO code application logic here
     }
     
